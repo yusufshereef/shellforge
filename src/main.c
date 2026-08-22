f3940d6a7e672802aa50b8224778819eac07a8d1
yusuf_shereef <yusufshereef69@gmail.com>
2026-08-22 10:35:16 +0530

    init

--- src/main.c
+++ src/main.c
@@ -6,23 +6,23 @@ char *line = NULL;
 size_t len = 0;
 ssize_t nread;
 while (1) {
-printf("shellforge$ ");
-fflush(stdout); // Force prompt to print immediately without buffer delays
-nread = getline(&line, &len, stdin);
-if (nread == -1) { // Detects Ctrl-D (End of File)
-printf("\nExiting cleanly...\n");
-break;
+    printf("shellforge$ ");
+    fflush(stdout); // Force prompt to print immediately without buffer delays
+    nread = getline(&line, &len, stdin);
+    if (nread == -1) { // Detects Ctrl-D (End of File)
+    printf("\nExiting cleanly...\n");
+    break;
 }
 // Strip the trailing newline character caused by pressing Enter
 if (nread > 0 && line[nread - 1] == '\n') {
-line[nread - 1] = '\0';
+    line[nread - 1] = '\0';
 }
 // Exit trap condition
 if (strcmp(line, "exit") == 0) {
-break;
+    break;
 }
 if (strlen(line) > 0) {
-printf("You typed: %s\n", line);
+    printf("You typed: %s\n", line);
 }
 }
 free(line); // Prevent memory leaks
